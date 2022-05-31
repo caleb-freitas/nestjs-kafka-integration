31e948318881acb8e32050b8e3ce40aa012b63c0
caleb-freitas <calebfreitas@tutanota.com>
2022-05-31 16:56:28 -0300

    docs: add api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
+  const config = new DocumentBuilder().setTitle('Nest.js Rest Api').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
   await app.listen(3000);
