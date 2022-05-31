2b02efc745ddca827853aaa0a79c102a31b49e4b
caleb-freitas <calebfreitas@tutanota.com>
2022-05-31 11:48:01 -0300

    chore: added orders resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { OrdersModule } from './orders/orders.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [],
+  imports: [OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
